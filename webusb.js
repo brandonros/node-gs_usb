3ae3ea38f395da74b02d7b175824da8a85e41881
Brandon Ros <brandonros1@gmail.com>
2019-11-12 20:03:07 -0500

    webusb sending

--- webusb.js
+++ webusb.js
@@ -117,6 +117,36 @@ const buf2hex = (buffer) => { // buffer is an ArrayBuffer
   return Array.prototype.map.call(new Uint8Array(buffer), x => ('00' + x.toString(16)).slice(-2)).join('');
 }
 
+const readLoop = async (device) => {
+  const endpointNumber = 0x01 // in
+  const length = 0xFF
+  const result = await device.transferIn(endpointNumber, length)
+  console.log(`< ${buf2hex(result.data.buffer)}`)
+  readLoop(device)
+}
+
+const send = async (device) => {
+  const endpointNumber = 0x02 // out
+  const arbitrationId = 0x7E5
+  const message = [0x02, 0x3E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+  const data = new ArrayBuffer(0x14)
+  const dataView = new DataView(data)
+  dataView.setUint32(0x00, 0xffffffff, true)
+  dataView.setUint16(0x04, arbitrationId, true)
+  dataView.setUint16(0x06, 0x0000, true)
+  dataView.setUint32(0x08, 0x00000008, true)
+  dataView.setUint8(0x0C, message[0])
+  dataView.setUint8(0x0D, message[1])
+  dataView.setUint8(0x0E, message[2])
+  dataView.setUint8(0x0F, message[3])
+  dataView.setUint8(0x10, message[4])
+  dataView.setUint8(0x11, message[5])
+  dataView.setUint8(0x12, message[6])
+  dataView.setUint8(0x13, message[7])
+  console.log(`> ${buf2hex(data)}`)
+  return device.transferOut(endpointNumber, data)
+}
+
 const init = async (deviceName) => {
   try {
     const device = await navigator.usb.requestDevice({
@@ -130,18 +160,19 @@ const init = async (deviceName) => {
     await device.selectConfiguration(configuration.configurationValue)
     await device.claimInterface(interface.interfaceNumber)
 
+    console.log(interface)
+
     await resetDevice(device)
     await sendHostConfig(device)
     const deviceConfig = await readDeviceConfig(device)
     const bitTimingConstants = await fetchBitTimingConstants(device)
     await startDevice(device)
-    alert(buf2hex(deviceConfig.data.buffer))
-  /*  for (;;) {
-      const endpointNumber = 0x01 // TODO?
-      const length = 0xFF
-      const result = await device.transferIn(endpointNumber, length)
-      console.log(result)
-    }*/
+
+    document.querySelector('#send').addEventListener('click', () => {
+      send(device)
+    })
+
+    readLoop(device)
   } catch (err) {
     alert(err)
   }
