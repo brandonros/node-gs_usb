224346a14f7ead254fc6063e7265de725e80ecf8
Brandon Ros <brandonros1@gmail.com>
2020-05-05 00:00:17 -0400

    new class interface

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const usb = require('usb')
 const util = require('util')
 const debug = require('debug')('node-gs_usb')
+const EventEmitter = require('events')
 
 const GS_USB_BREQ_HOST_FORMAT = 0
 const GS_USB_BREQ_MODE = 2
@@ -14,123 +15,100 @@ const USB_DIR_OUT = 0
 const USB_TYPE_VENDOR = (0x02 << 5)
 const USB_RECIP_INTERFACE = 0x01
 
-const setDeviceMode = (device, mode, flags) => {
-  debug('resetDevice')
-  const bmRequestType = USB_DIR_OUT | USB_TYPE_VENDOR | USB_RECIP_INTERFACE
-  const bRequest = GS_USB_BREQ_MODE
-  const wValue = 0 // https://github.com/torvalds/linux/blob/master/drivers/net/can/usb/gs_usb.c#L255
-  const wIndex = device.interfaces[0].descriptor.bInterfaceNumber
-  const data = Buffer.alloc(8)
-  data.writeUInt32LE(mode, 0) // mode
-  data.writeUInt32LE(flags, 4) // flags
-  return device.controlTransfer(
-    bmRequestType,
-    bRequest,
-    wValue,
-    wIndex,
-    data
-  )
-}
+const VENDOR_ID = 0x1D50
+const PRODUCT_ID = 0x606F
 
-const sendHostConfig = (device) => {
-  debug('sendHostConfig')
-  const bmRequestType =  USB_DIR_OUT | USB_TYPE_VENDOR | USB_RECIP_INTERFACE
-  const bRequest = GS_USB_BREQ_HOST_FORMAT
-  const wValue = 1 // https://github.com/torvalds/linux/blob/master/drivers/net/can/usb/gs_usb.c#L920
-  const wIndex = device.interfaces[0].descriptor.bInterfaceNumber
-  const data = Buffer.alloc(4)
-  data.writeUInt32LE(0xEFBE0000, 0)
-  return device.controlTransfer(
-    bmRequestType,
-    bRequest,
-    wValue,
-    wIndex,
-    data
-  )
-}
-
-const buildFrame = (arbitrationId, message) => {
-  const frameLength = 0x14
-  const data = new ArrayBuffer(frameLength)
-  const dataView = new DataView(data)
-  dataView.setUint32(0x00, 0xFFFFFFFF, true) // echo_id
-  dataView.setUint32(0x04, arbitrationId, true) // can_id
-  dataView.setUint8(0x08, 0x08) // can_dlc
-  dataView.setUint8(0x09, 0x00) // channel
-  dataView.setUint8(0x0A, 0x00) // flags
-  dataView.setUint8(0x0B, 0x00) // reserved
-  dataView.setUint8(0x0C, message[0])
-  dataView.setUint8(0x0D, message[1])
-  dataView.setUint8(0x0E, message[2])
-  dataView.setUint8(0x0F, message[3])
-  dataView.setUint8(0x10, message[4])
-  dataView.setUint8(0x11, message[5])
-  dataView.setUint8(0x12, message[6])
-  dataView.setUint8(0x13, message[7])
-  return Buffer.from(data)
-}
-
-const parseFrame = (frame) => {
-  const arbitrationId = frame.readUInt32LE(4)
-  const payload = frame.slice(12)
-  return {
-    arbitrationId,
-    payload
+class GsUsb extends EventEmitter {
+  setDeviceMode(mode, flags) {
+    debug('resetDevice')
+    const bmRequestType = USB_DIR_OUT | USB_TYPE_VENDOR | USB_RECIP_INTERFACE
+    const bRequest = GS_USB_BREQ_MODE
+    const wValue = 0 // https://github.com/torvalds/linux/blob/master/drivers/net/can/usb/gs_usb.c#L255
+    const wIndex = this.device.interfaces[0].descriptor.bInterfaceNumber
+    const data = Buffer.alloc(8)
+    data.writeUInt32LE(mode, 0) // mode
+    data.writeUInt32LE(flags, 4) // flags
+    return util.promisify(this.device.controlTransfer)(
+      bmRequestType,
+      bRequest,
+      wValue,
+      wIndex,
+      data
+    )
   }
-}
 
-const transferDataOut = (outEndpoint, frame) => {
-  debug(`transferDataOut frame=${frame.toString('hex')}`)
-  return new Promise((resolve, reject) => {
-    outEndpoint.transfer(frame, (err) => {
-      if (err) {
-        return reject(err)
-      }
-      resolve()
+  sendHostConfig() {
+    debug('sendHostConfig')
+    const bmRequestType = USB_DIR_OUT | USB_TYPE_VENDOR | USB_RECIP_INTERFACE
+    const bRequest = GS_USB_BREQ_HOST_FORMAT
+    const wValue = 1 // https://github.com/torvalds/linux/blob/master/drivers/net/can/usb/gs_usb.c#L920
+    const wIndex = this.device.interfaces[0].descriptor.bInterfaceNumber
+    const data = Buffer.alloc(4)
+    data.writeUInt32LE(0xEFBE0000, 0)
+    return util.promisify(this.device.controlTransfer)(
+      bmRequestType,
+      bRequest,
+      wValue,
+      wIndex,
+      data
+    )
+  }
+
+  async recv() {
+    for (;;) {
+      const frame = await util.promisify(this.inEndpoint.transfer)(32)
+      const arbitrationId = frame.readUInt32LE(4)
+      const data = frame.slice(12, 12 + 8)
+      this.emit('frame', {
+        arbitrationId,
+        data
+      })
+    }
+  }
+
+  async sendCanFrame(arbitrationId, data) {
+    const frameLength = 0x14
+    const frame = new ArrayBuffer(frameLength)
+    const dataView = new DataView(frame)
+    dataView.setUint32(0x00, 0xFFFFFFFF, true) // echo_id
+    dataView.setUint32(0x04, arbitrationId, true) // can_id
+    dataView.setUint8(0x08, 0x08) // can_dlc
+    dataView.setUint8(0x09, 0x00) // channel
+    dataView.setUint8(0x0A, 0x00) // flags
+    dataView.setUint8(0x0B, 0x00) // reserved
+    dataView.setUint8(0x0C, data[0])
+    dataView.setUint8(0x0D, data[1])
+    dataView.setUint8(0x0E, data[2])
+    dataView.setUint8(0x0F, data[3])
+    dataView.setUint8(0x10, data[4])
+    dataView.setUint8(0x11, data[5])
+    dataView.setUint8(0x12, data[6])
+    dataView.setUint8(0x13, data[7])
+    return util.promisify(this.outEndpoint.transfer)(Buffer.from(frame))
+  }
+
+  getUsbDevice() {
+    const deviceList = usb.getDeviceList()
+    const device = deviceList.find(device => {
+      return device.deviceDescriptor.idProduct === PRODUCT_ID &&
+        device.deviceDescriptor.idVendor === VENDOR_ID
     })
-  })
-}
-
-const transferDataIn = (inEndpoint, length) => {
-  return new Promise((resolve, reject) => {
-    inEndpoint.transfer(length, (err, frame) => {
-      if (err) {
-        return reject(err)
-      }
-      debug(`transferDataIn frame=${frame.toString('hex')}`)
-      resolve(frame)
-    })
-  })
-}
-
-const setupDevice = async (vendorId, productId) => {
-  //usb.setDebugLevel(4)
-  const deviceList = usb.getDeviceList()
-  const device = deviceList.find(device => {
-    return device.deviceDescriptor.idProduct === productId &&
-      device.deviceDescriptor.idVendor === vendorId
-  })
-  if (!device) {
-    throw new Error('Device not found')
+    if (!device) {
+      throw new Error('Device not found')
+    }
+    device.open()
+    device.interfaces[0].claim()
+    return device
   }
-  device.open()
-  device.interfaces[0].claim()
-  device.controlTransfer = util.promisify(device.controlTransfer)
-  const inEndpoint = device.interfaces[0].endpoints.find(endpoint => endpoint.constructor.name === 'InEndpoint')
-  const outEndpoint = device.interfaces[0].endpoints.find(endpoint => endpoint.constructor.name === 'OutEndpoint')
-  await setDeviceMode(device, GS_CAN_MODE_RESET, 0x00000000)
-  await sendHostConfig(device)
-  await setDeviceMode(device, GS_CAN_MODE_START, 0x00000000)
-  return {
-    inEndpoint,
-    outEndpoint
+
+  async init() {
+    this.device = this.getUsbDevice()
+    this.inEndpoint = this.device.interfaces[0].endpoints.find(endpoint => endpoint.constructor.name === 'InEndpoint')
+    this.outEndpoint = this.device.interfaces[0].endpoints.find(endpoint => endpoint.constructor.name === 'OutEndpoint')
+    await this.setDeviceMode(GS_CAN_MODE_RESET, 0x00000000)
+    await this.sendHostConfig()
+    await this.setDeviceMode(GS_CAN_MODE_START, 0x00000000)
   }
 }
 
-module.exports = {
-  setupDevice,
-  transferDataOut,
-  transferDataIn,
-  buildFrame,
-  parseFrame
-}
+module.exports = GsUsb
--- test.js
+++ test.js
@@ -1,41 +1,88 @@
-const {
-  setupDevice,
-  transferDataOut,
-  transferDataIn,
-  buildFrame
-} = require('./index')
+const proxyquire = require('proxyquire')
 
-const sendQueue = []
+let inTransferIndex = 0
+const inTransferMocks = [
+  Buffer.from('FFFFFFFF112233440800000011223344556677', 'hex')
+]
 
-const drainSendQueue = async (outEndpoint) => {
-  while (sendQueue.length) {
-    const frame = sendQueue.shift()
-    await transferDataOut(outEndpoint, frame)
-    console.log(`> ${frame.toString('hex')}`)
+const claim = () => {
+
+}
+
+const open = () => {
+
+}
+
+const controlTransfer = (bmRequestType, bRequest, wValue, wIndex, data, cb) => {
+  cb(null, undefined)
+}
+
+class InEndpoint {
+  constructor() {
+
+  }
+
+  transfer(length, cb) {
+    if (inTransferMocks[inTransferIndex]) {
+      setTimeout(() => {
+        cb(null, inTransferMocks[inTransferIndex])
+        inTransferIndex += 1
+      }, 1000)
+    }
   }
 }
 
-const run = async () => {
-  const vendorId = 0x1D50
-  const deviceId = 0x606F
-  const arbitrationId = 0x7E0
-  const message = [
-    0x03,
-    0x22,
-    0xF1,
-    0x21,
-    0x00,
-    0x00,
-    0x00,
-    0x00
-  ]
-  const { inEndpoint, outEndpoint } = await setupDevice(vendorId, deviceId)
-  sendQueue.push(buildFrame(arbitrationId, message))
-  for (;;) {
-    await drainSendQueue(outEndpoint)
-    const frame = await transferDataIn(inEndpoint, 0x14)
-    console.log(`< ${frame.toString('hex')}`)
+class OutEndpoint {
+  constructor() {
+
   }
+
+  transfer(frame, cb) {
+    console.log(`OutEndpoint: ${frame.toString('hex')}`)
+    cb(null, undefined)
+  }
+}
+
+const getDeviceList = () => {
+  return [
+    {
+      deviceDescriptor: {
+        idProduct: 0x606F,
+        idVendor: 0x1D50
+      },
+      controlTransfer,
+      interfaces: [
+        {
+          claim,
+          endpoints: [
+            new InEndpoint(),
+            new OutEndpoint()
+          ],
+          descriptor: {
+            bInterfaceNumber: 1
+          }
+        }
+      ],
+      open
+    }
+  ]
+}
+
+const GsUsb = proxyquire('./index.js', {
+  usb: {
+    getDeviceList
+  }
+})
+
+const run = async () => {
+  const gsUsb = new GsUsb(115200, 1000000)
+  gsUsb.on('frame', (frame) => {
+    console.log(frame)
+  })
+  await gsUsb.init()
+  gsUsb.sendCanFrame(0x11223344, Buffer.from('0700112233445566', 'hex'))
+  gsUsb.recv()
+  await new Promise(resolve => setTimeout(resolve, 1000))
 }
 
 run()
