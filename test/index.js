eb26f08b3f48b6e73a5279c88ec0398e7da961df
Brandon Ros <brandonros1@gmail.com>
2021-04-13 22:23:42 -0400

    better tests

--- test/index.js
+++ test/index.js
@@ -1,6 +1,15 @@
-const GsUsb = require('../index')
-document.querySelector('#go').addEventListener('click', async (event) => {
+const GsUsb = require('../index.js')
+
+const run = async () => {
   const gsUsb = new GsUsb()
   await gsUsb.init()
+  gsUsb.on('frame', (frame) => {
+    console.log(frame)
+  })
+  setInterval(async () => {
+    await gsUsb.sendCanFrame(0x7E0, Buffer.from('0210035555555555', 'hex'))
+  }, 1000)
   await gsUsb.recv()
-})
+}
+
+run()
